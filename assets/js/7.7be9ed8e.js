2b4b5a3d4bf92591609dd42074d6e7c5b72da62d
Alija Sabic <sabic.alija@gmail.com>
2020-01-23 11:05:05 +0100

    Publish

--- assets/js/7.7be9ed8e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{214:function(t,e,n){"use strict";n.r(e);var l={name:"Embed",props:{src:{type:String,default:null},id:{type:String,default:null}}},a=n(0),u=Object(a.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._t("default")],2)}),[],!1,null,"15aa5c9b",null);e.default=u.exports}}]);
